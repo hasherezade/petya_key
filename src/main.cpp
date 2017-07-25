69b4a90992f309ab6d4a1c70799daf2081afd2df
hasherezade <hasherezade@gmail.com>
2017-07-25 15:15:58 +0200

    [REFACT] Remove testing the private key

--- src/main.cpp
+++ src/main.cpp
@@ -29,44 +29,6 @@ uint8_t priv_bytes[] = {
     0x8b, 0xe1, 0x86, 0x54, 0xa2, 0x69, 0x5b, 0x47, 0x23
 };
 
-bool test_janus_privkey(EC_KEY *key)
-{
-    //the public key dumped from Petya:
-    const uint8_t pub_dumped_bytes[] = {
-        0x04, 0xc4, 0x80, 0xaf, 0x98, 0x2b, 0x11, 0x26, 0x9c, 0xb4, 0x38, 0xa0, 0x1c, 0x46, 0x79,
-        0xa8, 0x32, 0x9b, 0x5a, 0x5f, 0x4e, 0x80, 0x0c, 0x86, 0x9e, 0xa3, 0xd5, 0x26, 0x77, 0xf3,
-        0x26, 0x1e, 0xc8, 0x8d, 0xd1, 0x71, 0xec, 0xa5, 0xa9, 0x06, 0x6f, 0x4d, 0x8f, 0x26, 0xdc,
-        0xa6, 0x48, 0xfe, 0xf9
-    };
-    //public key:
-    size_t pub_len = i2o_ECPublicKey(key, NULL);
-    uint8_t *pub = (uint8_t*)calloc(pub_len, sizeof(uint8_t));
-
-    // fetch the public key in uncompressed form:
-    EC_KEY_set_conv_form(key, POINT_CONVERSION_UNCOMPRESSED);
-
-    // pub_copy is needed because i2o_ECPublicKey alters the input pointer   
-    uint8_t *pub_copy = pub;
-    if (i2o_ECPublicKey(key, &pub_copy) != pub_len) {
-        puts("Unable to decode public key");
-        return -1;
-    }
-
-    bbp_print_hex("derived pub  ", pub, pub_len);
-    bbp_print_hex("dumped pub   ", pub_dumped_bytes, sizeof(pub_dumped_bytes));
-    bool is_ok = false;
-    int res = memcmp(pub, pub_dumped_bytes, sizeof(pub_dumped_bytes));
-    if (res == 0) {
-        is_ok = true;
-        printf("[+] Test passed!\n[+] The public key dumped from the malware was derived from the published private key!\n");
-    } else {
-        printf("[-] Test failed!\n");
-    }
-    free(pub);
-    return is_ok;
-}
-
-
 void aes_decrypt_chunk(uint8_t enc_buf[AES_CHUNK_LEN], uint8_t *key_bytes)
 {
     AES_KEY key;
